02a1514fc5e2442d94cb27fdcdd094bc4d30da4f
MaremianaComp <139053449+MaremianaComp@users.noreply.github.com>
2023-07-13 21:57:21 +0200

    Add files via upload

---
+++ 2023_06_26_170135_create_documents_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->index('created_at');
+
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+
+            $table->string('years');
+            $table->string('name');
+            $table->string('archive');
+            $table->string('url');
+
+            $table->boolean('published')->default(true);
+            $table->timestamp('published_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('documents');
+    }
+};
---
+++ 2023_06_27_133026_create_document_place_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('document_place', function (Blueprint $table): void {
+            $table->foreignId('document_id')->index()->constrained()->cascadeOnDelete();
+            $table->foreignId('place_id')->index()->constrained()->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('document_place');
+    }
+};
