09480e74c28cd140af7f088f7b2850de36c29f5b
MaremianaComp <139053449+MaremianaComp@users.noreply.github.com>
2023-07-13 22:41:29 +0200

    Update DocumentController.php

--- DocumentController.php
+++ DocumentController.php
@@ -7,35 +7,14 @@ use Illuminate\Http\Request;
 
 class DocumentController extends Controller
 {
-    public function index() {
-        // $documents = Document::latest()->get();
-        // $s = "Документы\r\n\r\n";
-        // foreach ($documents as $document) {
-            
-        // $s .= $document->name . "\r\n";
-        // $s .= $document->archive . "\r\n";
-        // $s .= $document->url . "\r\n";
-        // $s .= $document->created_at . "\r\n";
-        // $s .= "\r\n";
-        // }
-        // return response($s) 
-        // ->header('Content-Type', 'text/plain');
-
+    public function index() 
+    {
         $context = ['documents' => Document::latest()->get()];
         return view('index', $context);
-        }
+    }
 
-    public function detail(Document $document) {
-        // $document = Document::find($document);
-        
-        // $s = $document->name . "\r\n\r\n";
-        // $s .= $document->years . "\r\n";
-        // $s .= $document->archive . "\r\n";
-        // $s .= $document->url . "\r\n";
-        // $s .= $document->created_at . "\r\n";
-        // return response($s)->header('Content-Type', 'text/plain');
-        // }
-
+    public function detail(Document $document) 
+    {
         return view('detail', ['document' => $document]);
     }
 }
