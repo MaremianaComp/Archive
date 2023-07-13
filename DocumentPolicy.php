12d7cbc136a6e1efdc4b1a7478a9a2b2b45abad3
MaremianaComp <139053449+MaremianaComp@users.noreply.github.com>
2023-07-13 21:41:08 +0200

    Update DocumentPolicy.php

--- DocumentPolicy.php
+++ DocumentPolicy.php
@@ -10,23 +10,11 @@ class DocumentPolicy
 {
     use HandlesAuthorization;
 
-    /**
-     * Create a new policy instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
-
     public function update(User $user, Document $document): bool
     {
         return $document->user()->is($user);
     }
-    // public function update(User $user, Document $document) {
-    //     return $document->user->id === $user->id;
-    //     }
+   
     public function destroy(User $user, Document $document) 
     {
         return $this->update($user, $document);
