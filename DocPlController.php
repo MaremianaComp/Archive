1c83afa08f9ac1c20c75a9f518f0beeeeb581e36
MaremianaComp <139053449+MaremianaComp@users.noreply.github.com>
2023-07-13 15:02:21 +0200

    Update DocPlController.php

--- DocPlController.php
+++ DocPlController.php
@@ -8,26 +8,8 @@ use Illuminate\Http\Request;
 
 class DocPlController extends Controller
 {
-    public function index(Place $place 
-   
-    )
+    public function index(Place $place)
     {   
-        
-        return view('place', compact('place', 
-        
-        )
-    
-    );
-       
-        }
-
-        
-    }
-
-    
-
-   
-   
-    //     return Place::find($place_id)->documents;
-    // }
-// }
+        return view('place', compact('place',));
+    }   
+}
