0b215798002ff290993a0fd3dcc0ff09937a00fc
MaremianaComp <139053449+MaremianaComp@users.noreply.github.com>
2023-07-28 21:45:25 +0200

    Update DocumentController.php

--- DocumentController.php
+++ DocumentController.php
@@ -16,19 +16,5 @@ class DocumentController extends Controller
     public function detail(Document $document) 
     {
         return view('detail', ['document' => $document]);
-    }
-
-    public function autoCompleteSearch(Request $request): JsonResponse
-    {
-        $data = [];
-    
-        if($request->filled('q')){
-            $data = User::select("name", "id")
-                        ->where('name', 'LIKE', '%'. $request->get('q'). '%')
-                        ->get();
-        }
-     
-        return response()->json($data);
-    }
-    
+    }   
 }
